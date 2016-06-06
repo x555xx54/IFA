af08ecbd310bbed306ddda6e3c13bac3be041acf
x555xx54 <x555xx@bk.ru>
2016-06-06 21:02:05 +0600

    test2

---
+++ app/src/main/java/com/example/kiril/micvol/PhoneStateReceiver.java
@@ -0,0 +1,15 @@
+package com.example.kiril.micvol;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by kir on 06.06.2016.
+ */
+public class PhoneStateReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
