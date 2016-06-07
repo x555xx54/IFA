2e53c03f6a600818d927ce97db31fc11bc91720c
x555xx54 <x555xx@bk.ru>
2016-06-07 22:26:43 +0600

    first commit

--- app/src/main/java/com/example/kiril/micvol/PhoneStateReceiver.java
+++ app/src/main/java/com/example/kiril/micvol/PhoneStateReceiver.java
@@ -3,6 +3,12 @@ package com.example.kiril.micvol;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import static com.example.kiril.micvol.Main.LOG_TAG;
+
 
 /**
  * Created by kir on 06.06.2016.
@@ -10,6 +16,18 @@ import android.content.Intent;
 public class PhoneStateReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        try {
+            TelephonyManager tmgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 
+        }
+    }
+    class MyPhoneStateListener extends PhoneStateListener{
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            if (state==1){
+                Log.d(LOG_TAG, "MyPhoneListener");
+            }
+        }
     }
 }
